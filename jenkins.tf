5da5f0b63388e8a4b003d68ce797941483f73215
Adiel <adiel@newit.co.il>
2019-09-20 13:53:07 +0300

    Create Jenkins user data

--- jenkins.tf
+++ jenkins.tf
@@ -3,7 +3,31 @@ resource "aws_instance" "jenkins-instance" {
     
     ami = "${lookup(var.ECS_AMI, var.AWS_REGION)}"
     instance_type = "t2.micro"
-
+    user_data = <<EOF
+        #! /bin/bash
+        sudo su
+        sudo yum -y install wget zip unzip file
+        yum -y update
+        vgchange -ay
+        mkdir -p /var/lib/jenkins
+        echo '/dev/data/volume1 /var/lib/jenkins ext4 defaults 0 0' >> /etc/fstab
+        yum -y update
+        yum -y install java-1.8.0-openjdk
+        sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+        sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+        sudo yum -y install jenkins
+        sudo yum -y install docker
+        export PATH=/root/.local/bin:$PATH
+        sudo yum -y install python36
+        wget -q https://bootstrap.pypa.io/get-pip.py
+        python3 get-pip.py --user
+        pip3 install awscli --user
+        curl -L https://raw.githubusercontent.com/warrensbox/terraform-switcher/release/install.sh | bash
+        export PATH=/usr/local/bin/:$PATH
+        sudo service jenkins start
+        sudo chkconfig jenkins on
+    EOF
+    
     # VPC Subnet
     subnet_id = "${aws_subnet.floor1-public-subnet.id}"
 
